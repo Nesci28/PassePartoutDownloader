aad2a23768f8cd6e6b3155cdc6fa5dd32f8bf31d
Nesci <markgagnon28@gmail.com>
2020-11-20 19:50:40 -0500

    update: added episode date

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const puppeteer = require("puppeteer");
 const { execSync } = require("child_process");
+const axios = require("axios");
 
 const FIRSTEPISODEINDEX = 49;
 
@@ -31,7 +32,12 @@ const FIRSTEPISODEINDEX = 49;
   for (let i = FIRSTEPISODEINDEX; i < videos.length; i++) {
     const video = videos[i];
     console.log(`${i}/${final}`);
-    const name = video.split("/").pop();
+    const name = video.split("/").pop().replace(/-/g, " ");
+    const id = video.split("/videos/").pop().split("/")[0];
+    const { data } = await axios.get(
+      `https://mnmedias.api.telequebec.tv/api/v4/player/${id}`
+    );
+    const date = convertToDate(data);
 
     let videoPage = await browser.newPage();
     await videoPage.goto(video, {
@@ -50,9 +56,33 @@ const FIRSTEPISODEINDEX = 49;
       )
     );
     execSync(
-      `youtube-dl http://players.brightcove.net/${dataAccount}/default_default/index.html?videoId=${videoDataId} -o videos/${name}.%(ext)s`
+      `youtube-dl http://players.brightcove.net/${dataAccount}/default_default/index.html?videoId=${videoDataId} -o "videos/${date}.${name}.%(ext)s"`
     );
   }
 
   await browser.close();
 })();
+
+function convertToDate(data) {
+  const months = [
+    "janvier",
+    "février",
+    "mars",
+    "avril",
+    "mai",
+    "juin",
+    "juillet",
+    "août",
+    "septembre",
+    "octobre",
+    "novembre",
+    "décembre",
+  ];
+  const startDateText =
+    data.userRequestContext.canalDiffusion.broadcastDates.current.startDateText;
+  const dateElements = startDateText.split(" ");
+  const day = dateElements[0];
+  const month = months.indexOf(dateElements[1]) + 1;
+  const year = dateElements[2];
+  return `${year}-${month}-${day}`;
+}
